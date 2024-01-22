3569e0311d0dc00eb69cf26c3c74040dc2d7bdd6
fynnjh3708 <31434492-fynnjh3708@users.noreply.replit.com>
2024-01-22 05:09:34 +0000

    Test2

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("Hello World!")
+import pygame, sys
+from pygame.locals import *
+pygame.init()
+
+pygame.display.set_caption("Hello World!")
+Screen = pygame.display.set_mode((500, 500))
+
+xpos = 50
+clock = pygame.time.Clock()
+# Screen.fill((255, 255, 255))
+
+while 1:
+  clock.tick(100)
+  
+  for event in pygame.event.get():
+    if event.type == QUIT:
+      sys.exit()
+
+  pressed_keys = pygame.key.get_pressed()
+
+  if pressed_keys[K_RIGHT] == 1:
+    xpos += 1
+  if pressed_keys[K_LEFT] == 1:
+    xpos -= 1
+  
+  Screen.fill((255, 255, 255))
+  pygame.draw.circle(Screen, (0, 255, 0), (xpos, 200), 20)
+
+  pygame.display.update()
